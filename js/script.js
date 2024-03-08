10a8b09aef576c3cc45aa6ae6cdc9c0a89118b27
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-03-08 10:16:23 +0100

    add bonus 1

--- js/script.js
+++ js/script.js
@@ -2,29 +2,31 @@
 // ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
 
+const numberContainer = document.querySelector('#container')
+
 // STAMPO I NUMNERI DA 1 A 100 
 for(let i = 1; i <=100; i++) {
     // console.log(i);
     
-    // let fizOrBuzz ;
-
+    let message ;
     // SE MULTIPLI SIA DI 3 CHE DI 5 SCRIVO FIZZBUZZ
     if (i % 5 === 0 && i % 3 === 0) {
-        // fizOrBuzz = i + ` fizzBuzz`;
-        console.log('fizOrBuzz')
+        message = 'fizzOrBuzz';
     }
 
     // SE MULTIPLI DI 3 SCRIVO FIZZ
     else if (i % 3 === 0) {
-        // fizOrBuzz = i + ` fizz`;
-        console.log('fizz')
+        message = 'fizz';
     }
     // SE MULTIPLI DI 5 SCRIVO BUZZ
      else if (i % 5 === 0) {
-        // fizOrBuzz = i + ` buzz`;
-        console.log('Buzz')
+        message = 'Buzz';
     }
     else {
-        console.log(i)
+        message = i
     }
+
+    // console.log(message);
+    const newBox = `<div class="box">${message}</div>`;
+    numberContainer.innerHTML += newBox;
 }
